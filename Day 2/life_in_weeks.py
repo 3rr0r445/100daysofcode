154d784dda33558b9d2f2c904f99a510a0a36165
3rr0r445 <TravisEDuran@gmail.com>
2025-06-25 11:12:39 -0400

    Fix calculations in life_in_weeks.py to correctly compute remaining days, weeks, and months based on user age

--- Day 2/life_in_weeks.py
+++ Day 2/life_in_weeks.py
@@ -4,10 +4,10 @@ max_age = 90
 answer_age = max_age - age
 
 # Define variables for months, weeks, days using mathmatical operators
-answer_months = answer_age // 12
-answer_weeks = answer_age // 52
-answer_days = answer_age //365
+answer_months = answer_age * 12
+answer_weeks = answer_age * 52
+answer_days = answer_age * 365
 
 # Print the results using an F-String
-print(f"You have {answer_days}, {answer_weeks}, and {answer_months} left.")
+print(f"You have {answer_days} days, {answer_weeks} weeks, and {answer_months} months left.")
 
