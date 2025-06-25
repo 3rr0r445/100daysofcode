72e0395a1c7d1286e51615c1b228e0c17860754a
3rr0r445 <TravisEDuran@gmail.com>
2025-06-25 10:54:48 -0400

    Add Band Name Generator, BMI calculator, and two-digit number addition scripts

---
+++ Day 1/bandnamegenerator.py
@@ -0,0 +1,9 @@
+# Day 1 of 100 days of Code Python
+# Band Name Generator
+# I'm practically skipping this day because I know this much.
+
+print("Welcome to the Band Name Generator")
+city = input("What's the name of the city you grew up in?\n")
+pet = input("What's the name of your pet?\n")
+
+print(f"Your band name could be {city} {pet}")
