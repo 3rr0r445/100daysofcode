26469883c319acf6a1f5f454c2ae01f010af8218
3rr0r445 <TravisEDuran@gmail.com>
2025-06-25 10:58:44 -0400

    Add life_in_weeks.py script to calculate remaining days, weeks, and months based on user age

---
+++ Day 2/life_in_weeks.py
@@ -0,0 +1,13 @@
+# Prompt user for their age and compare to max age (90)
+age = int(input("What is your current age? "))
+max_age = 90
+answer_age = max_age - age
+
+# Define variables for months, weeks, days using mathmatical operators
+answer_months = answer_age // 12
+answer_weeks = answer_age // 52
+answer_days = answer_age //365
+
+# Print the results using an F-String
+print(f"You have {answer_days}, {answer_weeks}, and {answer_months} left.")
+
