87c3054dcd34d11de6ee4ff70c886a5f5909bd1d
3rr0r445 <TravisEDuran@gmail.com>
2025-12-16 17:02:12 -0500

    Refactor input handling in high score script to split scores into a list

--- Day 5/high_score.py
+++ Day 5/high_score.py
@@ -1,5 +1,11 @@
-student_scores = input("Input a list of student scores. ")
+student_scores = input("Input a list of student scores. ").split()
 for n in range(0, len(student_scores)):
     student_scores[n] = int(student_scores[n])
 print(student_scores)
+
+# Starting Code Above. Do Not Code Above This Line!!
+
+# Write a program that calculates the highest value in the list
+# Do not use the max function to do it easily. 
+# Probably will use a loop and conditionals.
 
