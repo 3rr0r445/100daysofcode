4628df0161f5e1893688a8fa0503b361ba26b227
3rr0r445 <TravisEDuran@gmail.com>
2025-12-16 17:34:21 -0500

    Refactor high score calculation to include two methods for finding the maximum score and improve comments for clarity

--- Day 5/high_score.py
+++ Day 5/high_score.py
@@ -11,8 +11,17 @@ print(student_scores)
 
 max_value = student_scores[0]
 
-# No conditionals. Just a for loop.
+# No conditionals. Just a for loop. This one uses list slicing
+# It sets the default value for max_value as the first value in the list
+# It uses list slicing to start iterating at the second list item
 for n in student_scores[1:]:
     if n > max_value:
         max_value = n
-    print(max_value)
+print(f"The highest score in the class is {max_value}!")
+
+# This one doesn't use list slicing, just the entire list.
+for n in student_scores:
+    if n > max_value:
+        max_value = n
+
+print(f"The highest score in the class is {max_value}!")
